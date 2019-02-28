dcab75ef4e351a5fa182d5e1c1f517d0733ad406
Олег Цыганов <olegcyganov@MacBook-Pro-Oleg.local>
2019-02-28 23:54:28 +0400

    service

---
+++ js/service.js
@@ -0,0 +1,20 @@
+var services = document.querySelectorAll(".service-info");
+var servicesNav = document.querySelector(".service-list");
+var servicesTab = document.querySelectorAll(".service-item");
+
+servicesNav.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    var r = evt.target;
+    if (r.classList.contains("service-link")) {
+        for (var i = 0; i < services.length; i++) {
+            services[i].classList.remove("service-info__current");
+            servicesTab[i].classList.remove("service-link__current");
+        }
+        var t = r.parentElement;
+        var s = ".service-information-" + r.id.split("-")[0];
+        document.querySelector(s).classList.add("service-info__current");
+        t.classList.add("service-link__current");
+    }
+});
+
+
