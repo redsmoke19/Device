1333eb8dbbd4b980f9d13bb39cdc4017e34c461e
redsmoke19 <44342965+redsmoke19@users.noreply.github.com>
2019-03-01 15:02:54 +0400

    'footer'

--- js/service.js
+++ js/service.js
@@ -1,19 +1,20 @@
-var services = document.querySelectorAll(".service-info");
-var servicesNav = document.querySelector(".service-list");
-var servicesTab = document.querySelectorAll(".service-item");
+var services = document.querySelectorAll('.service-info');
+var servicesNav = document.querySelector('.service-list');
+var servicesTab = document.querySelectorAll('.service-item');
 
-servicesNav.addEventListener("click", function(evt) {
+servicesNav.addEventListener('click', function(evt) {
     evt.preventDefault();
     var r = evt.target;
-    if (r.classList.contains("service-link")) {
+    if (r.classList.contains('service-link')) {
         for (var i = 0; i < services.length; i++) {
-            services[i].classList.remove("service-info__current");
-            servicesTab[i].classList.remove("service-link__current");
+            services[i].classList.remove('service-info__current');
+            servicesTab[i].classList.remove('service-link__current');
         }
         var t = r.parentElement;
-        var s = ".service-information-" + r.id.split("-")[0];
-        document.querySelector(s).classList.add("service-info__current");
-        t.classList.add("service-link__current");
+        var s = '.service-information-' + r.id.split('-')[0];
+        var serviceInfo = document.querySelector(s);
+        serviceInfo.classList.add('service-info__current');
+        t.classList.add('service-link__current');
     }
 });
 
